a3936a2ca2e75fa76fc36e2dbb3470c99b29fe96
rje1974 <juaneduardoriva@gmail.com>
2022-01-23 16:11:38 -0500

    primer commit

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  var el = document.getElementById("o")
+  el.remove();
+
+  // y añade contenido
+  var newDiv = document.createElement("li");
+  console.log(Object.values(cosasQueAprendimos)[1].tema)
+  var newContent = document.createTextNode(cosasQueAprendimos.keys);
+  newDiv.appendChild(newContent); //añade texto al div creado.
+
+  // añade el elemento creado y su contenido al DOM
+  var currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+
+
+}
 
 main();
